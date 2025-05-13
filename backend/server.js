e6520e8cfc9bd7a3fe1080d0d515c35acd4f8ce8
PatV3-0 <130934721+PatV3-0@users.noreply.github.com>
2025-05-13 15:41:26 +0200

    Initial api for users

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
+const { json } = require('stream/consumers');
 
 const app = express();
 const PORT = 3001;
@@ -12,11 +13,68 @@ const loadJSON = (filename) => {
   return JSON.parse(fs.readFileSync(`./data/${filename}`, 'utf8'));
 };
 
-app.get('/api/users', (_, res) => res.json(loadJSON('users.json')));
 app.get('/api/simulations', (_, res) => res.json(loadJSON('simulations.json')));
 app.get('/api/intersections', (_, res) => res.json(loadJSON('intersections.json')));
 app.get('/api/optimizations', (_, res) => res.json(loadJSON('optimizations.json')));
 app.get('/api/traffic', (_, res) => res.json(loadJSON('traffic_data.json')));
+
+//user routes
+function loadUsers(){
+    const raw = fs.readFileSync('./data/users.json');
+    const data = JSON.parse(raw);
+    return data.users;
+}
+
+app.get('/users', (req, res) => {
+    const users = loadUsers();
+    res.json(users);
+});
+
+app.get('/users/:id', (req, res) => {
+    const users = loadUsers();
+    const user = users.find(u => u.user_id === req.params.id);
+    if(!user){
+        return res.status(404).json({error: 'User not found.'});
+    }
+    res.json(user);
+})
+
+app.get('/users/:id/simulations', (req, res) => {
+    const users = loadUsers();
+    const user = users.find(u => u.user_id === req.params.id);
+    if(!user){
+        return res.status(404).json({error: 'User not found.'});
+    }
+    res.json({simulations: user.simulations});
+})
+
+app.post('/users', (req, res) => {
+    const users = loadUsers();
+    const newUser = req.body;
+    const lastUser = users[users.length - 1];
+    const lastUserId = lastUser ? lastUser.user_id : "u000"
+    const lastUserNumber = parseInt(lastUserId.slice(1));
+    const newUserId = `u${(lastUserNumber + 1).toString().padStart(3, '0')}`;
+    const defaultValues = {
+        user_id: newUserId,
+        name: "",
+        lastName: "",
+        userName: "",
+        email: "",
+        role: "analyst",
+        profileArt: "https://www.google.com/imgres?q=placeholder%20image&imgurl=https%3A%2F%2Fwww.canbind.ca%2Fwp-content%2Fuploads%2F2025%2F01%2Fplaceholder-image-person-jpg.jpg&imgrefurl=https%3A%2F%2Fwww.canbind.ca%2Fabout-can-bind%2Four-team%2Fexecutive-committee%2Fplaceholder-image-person-jpg%2F&docid=-btyGbUwWLtnKM&tbnid=Cn7x48J0PlsGsM&vet=12ahUKEwjJ0JTSxaCNAxVvV0EAHY8xEkAQM3oECFYQAA..i&w=820&h=678&hcb=2&ved=2ahUKEwjJ0JTSxaCNAxVvV0EAHY8xEkAQM3oECFYQAA",
+        created_at: new Date().toISOString(),
+        simulations: [],
+        auth: "",
+    };
+    const userWithDefaults = {...defaultValues, ...newUser};
+    if(!userWithDefaults.user_id || !userWithDefaults.userName){
+        return res.status(400).json({error: 'User id and username required.'});
+    } 
+    users.push(userWithDefaults);
+    fs.writeFileSync('./data/users.json', JSON.stringify({users}, null, 2));
+    res.status(201).json(userWithDefaults);
+})
 
 app.listen(PORT, () => {
   console.log(`✅ Backend API running on http://localhost:${PORT}`);
