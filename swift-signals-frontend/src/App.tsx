9047de11723ae32c7596d3c0c00d562ed5f5757e
Joshua Garner <joshua.garner@tuks.co.za>
2025-05-16 20:46:59 +0200

    Login Page Added

--- swift-signals-frontend/src/App.tsx
+++ swift-signals-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import ErrorBoundary from './components/ErrorBoundary';
 import reactLogo from './assets/react.svg';
@@ -11,10 +12,12 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <ErrorBoundary>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='/logout' element={<Login />} />
         </Routes>
       </ErrorBoundary>
       <Footer />
