e500b50c88d92ceeabaa153a84870fcae881f33b
KgosiSegale <u22534718@tuks.co.za>
2025-05-22 14:41:51 +0200

    importing component

--- frontend/src/pages/Users.tsx
+++ frontend/src/pages/Users.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Navbar from '../components/Navbar';
+import UsersTable from '../components/UsersTable';
 
 // TypeScript interface for user data
 interface User {
