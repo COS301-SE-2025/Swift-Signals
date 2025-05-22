018516cd63bcb0572b714f1bd0f663a7d541f5d1
KgosiSegale <u22534718@tuks.co.za>
2025-05-22 14:15:06 +0200

    Update IntersectionCard.tsx

--- frontend/src/components/IntersectionCard.tsx
+++ frontend/src/components/IntersectionCard.tsx
@@ -1,1 +1,13 @@
+interface IntersectionCardProps {
+  id: number;
+  name: string;
+  location: string;
+  lanes: string;
+  image?: string;
+  onSimulate: (id: number) => void;
+  onEdit: (id: number) => void;
+  onDelete: (id: number) => void;
+}
 
+
+export default IntersectionCard;
