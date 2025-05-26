20dfe625a1d224e33bf0b1e34745631f0bf66572
Chris <christopherxides@gmail.com>
2025-05-26 15:37:41 +0200

    fixed port bug

--- scripts/frontend-dev.ps1
+++ scripts/frontend-dev.ps1
@@ -1,14 +1,14 @@
 Write-Host "Building the Docker image..."
-docker build --no-cache -t frontend:dev -f ../build/package/Dockerfile.frontend ../
+docker build --no-cache -t swift-signals-frontend:latest -f ../build/package/Dockerfile.frontend ../
 
 Write-Host "Loading the image into Minikube..."
-minikube image load frontend:dev
+minikube image loadswift-signals-frontend:latest 
 
 Write-Host "Applying Kubernetes manifests..."
 kubectl apply -f ../deployments/k8s/development/frontend.yaml
 
 Write-Host "Connecting port 30080 to localhost..."
-Start-Process -NoNewWindow kubectl port-forward svc/frontend 30080:80
+kubectl port-forward svc/frontend 30080:80
 
 Write-Host "Frontend available at http://localhost:30080"
 
--- scripts/frontend-dev.sh
+++ scripts/frontend-dev.sh
@@ -3,16 +3,16 @@
 set -e
 
 echo "Building the Docker image..."
-docker build --no-cache -t frontend:dev -f ../build/package/Dockerfile.frontend ../
+docker build --no-cache -t swift-signals-frontend:latest -f ../build/package/Dockerfile.frontend ../
 
 echo "Loading the image into Minikube..."
-minikube image load frontend:dev
+minikube image load swift-signals-frontend:latest
 
 echo "Applying Kubernetes manifests..."
 kubectl apply -f ../deployments/k8s/development/frontend.yaml
 
 echo "Connecting port 30080 to localhost..."
-kubectl port-forward svc/frontend 30080:5173 &
+kubectl port-forward svc/frontend 30080:80 
 
 echo "Frontend available at http://localhost:30080"
 
