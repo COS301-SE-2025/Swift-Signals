1b2dca68d4182eebb739d161c0a560b949862fd8
PatV3-0 <130934721+PatV3-0@users.noreply.github.com>
2025-06-21 16:52:25 +0200

    Updated imports

--- Sumo_Project_Breakdown/tests/test_MainSim.py
+++ Sumo_Project_Breakdown/tests/test_MainSim.py
@@ -2,10 +2,7 @@ import sys
 import unittest
 from unittest.mock import patch, mock_open
 import uuid
-import subprocess
 import pathlib
-import tempfile
-import os
 
 if "SimLoad" not in sys.modules:
     sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent))
