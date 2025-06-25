1470c523266cbb6162be32ca264901c10f7c7467
Shravan Seebran <shravanseebran@gmail.com>
2025-06-25 23:03:22 +0200

    Changed interface to take in integers for IDs

--- user-service/internal/db/user_db_interface.go
+++ user-service/internal/db/user_db_interface.go
@@ -8,10 +8,10 @@ import (
 // Repository interface defines the contract for user data operations
 type UserRepository interface {
 	CreateUser(ctx context.Context, user *model.UserResponse) (*model.UserResponse, error)
-	GetUserByID(ctx context.Context, id string) (*model.UserResponse, error)
+	GetUserByID(ctx context.Context, id int) (*model.UserResponse, error)
 	GetUserByEmail(ctx context.Context, email string) (*model.UserResponse, error)
 	UpdateUser(ctx context.Context, user *model.UserResponse) (*model.UserResponse, error)
-	DeleteUser(ctx context.Context, id string) error
+	DeleteUser(ctx context.Context, id int) error
 	ListUsers(ctx context.Context, limit, offset int) ([]*model.UserResponse, error)
 	AddIntersectionID(ctx context.Context, userID int, intID int) error
 	GetIntersectionsByUserID(ctx context.Context, userID int) ([]int, error)
