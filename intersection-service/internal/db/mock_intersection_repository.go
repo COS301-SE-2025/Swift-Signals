7f211a550e92d8ec01bb312f34a5978f203191c2
Chris <christopherxides@gmail.com>
2025-06-25 18:48:36 +0200

    Created intersection database interface and mock implementation

--- intersection-service/internal/db/mock_intersection_repository.go
+++ intersection-service/internal/db/mock_intersection_repository.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"context"
+	"github.com/COS301-SE-2025/Swift-Signals/intersection-service/internal/model"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -13,4 +15,28 @@ func NewIntersectionRepository() IntersectionRepository {
 	return &MockRepository{
 		Mock: mock.Mock{},
 	}
+}
+
+func (m *MockRepository) CreateIntersection(ctx context.Context, intersection *model.IntersectionResponse) (*model.IntersectionResponse, error) {
+	args := m.Called(ctx, intersection)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*model.IntersectionResponse), args.Error(1)
+}
+
+func (m *MockRepository) GetIntersectionByID(ctx context.Context, id string) (*model.IntersectionResponse, error) {
+	args := m.Called(ctx, id)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*model.IntersectionResponse), args.Error(1)
+}
+
+func (m *MockRepository) GetAllIntersections(ctx context.Context, user_id string) ([]*model.IntersectionResponse, error) {
+	args := m.Called(ctx, user_id)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).([]*model.IntersectionResponse), args.Error(1)
 }
