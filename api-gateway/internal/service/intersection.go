b19c369a6d4baf4bd18772b15d0de0f5803e057c
Shravan Seebran <shravanseebran@gmail.com>
2025-06-25 19:50:37 +0200

    Implement intersection services logic

--- api-gateway/internal/service/intersection.go
+++ api-gateway/internal/service/intersection.go
@@ -2,9 +2,14 @@ package service
 
 import (
 	"context"
+	"errors"
+	"io"
+	"strconv"
 
 	"github.com/COS301-SE-2025/Swift-Signals/api-gateway/client"
 	"github.com/COS301-SE-2025/Swift-Signals/api-gateway/internal/model"
+	"github.com/COS301-SE-2025/Swift-Signals/api-gateway/internal/util"
+	"github.com/COS301-SE-2025/Swift-Signals/protos/gen/intersection"
 )
 
 type IntersectionService struct {
@@ -17,16 +22,59 @@ func NewIntersectionService(ic *client.IntersectionClient) *IntersectionService 
 	}
 }
 
-func (s *IntersectionService) GetAllIntersections(ctx context.Context) (resp model.Intersections, err error) {
-	return model.Intersections{}, nil
+func (s *IntersectionService) GetAllIntersections(ctx context.Context) (model.Intersections, error) {
+	stream, err := s.intrClient.GetAllIntersections(ctx)
+	if err != nil {
+		return model.Intersections{}, errors.New("Unable to get all intersections")
+	}
+
+	intersections := []model.Intersection{}
+	for {
+		rpcIntersection, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return model.Intersections{}, errors.New("Unable to get all intersections")
+		}
+		intersection := util.RPCIntersectionToIntersection(rpcIntersection)
+		intersections = append(intersections, intersection)
+	}
+
+	resp := model.Intersections{Intersections: intersections}
+	return resp, nil
 }
 
-func (s *IntersectionService) GetIntersectionByID(ctx context.Context, id int) (resp model.Intersection, err error) {
-	return model.Intersection{}, nil
+func (s *IntersectionService) GetIntersectionByID(ctx context.Context, id int) (model.Intersection, error) {
+	pbResp, err := s.intrClient.GetIntersection(ctx, strconv.Itoa(id))
+	if err != nil {
+		return model.Intersection{}, errors.New("Unable to get all intersections")
+	}
+
+	resp := util.RPCIntersectionToIntersection(pbResp)
+
+	return resp, nil
 }
 
-func (s *IntersectionService) CreateIntersection(ctx context.Context, req model.CreateIntersectionRequest) (resp model.CreateIntersectionResponse, err error) {
-	return model.CreateIntersectionResponse{}, nil
+func (s *IntersectionService) CreateIntersection(ctx context.Context, req model.CreateIntersectionRequest) (model.CreateIntersectionResponse, error) {
+	intersection := model.Intersection{
+		Name:           req.Name,
+		Details:        req.Details,
+		TrafficDensity: req.TrafficDensity,
+		DefaultParameters: model.OptimisationParameters{
+			SimulationParameters: req.DefaultParameters,
+		},
+	}
+	pbResp, err := s.intrClient.CreateIntersection(ctx, intersection)
+	if err != nil {
+		return model.CreateIntersectionResponse{}, errors.New("Unable to get all intersections")
+	}
+
+	resp := model.CreateIntersectionResponse{
+		Id: pbResp.Id,
+	}
+
+	return resp, nil
 }
 
 func (s *IntersectionService) UpdateIntersectionByID(ctx context.Context, id int, req model.UpdateIntersectionRequest) (resp model.Intersection, err error) {
