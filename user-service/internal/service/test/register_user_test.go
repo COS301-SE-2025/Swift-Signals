a484191c31279337be2e0c80c8b77c32e2c25ee3
Chris <christopherxides@gmail.com>
2025-07-27 17:51:45 +0200

    Implement user-service service RegisterUser endpoint unit test

--- user-service/internal/service/test/register_user_test.go
+++ user-service/internal/service/test/register_user_test.go
@@ -1,14 +1,181 @@
 package test
 
 import (
+	"context"
+	"errors"
 	"testing"
 
+	errs "github.com/COS301-SE-2025/Swift-Signals/shared/error"
+	"github.com/COS301-SE-2025/Swift-Signals/user-service/internal/model"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 )
 
-func (suite *TestSuite) TestRegisterUser() {
-	// TODO: Implement the unit test for TestRegisterUser
-	suite.True(true)
+func (suite *TestSuite) TestRegisterUser_Success() {
+
+	email := "valid@gmail.com"
+	name := "Valid Name"
+	plainPassword := "8characters"
+
+	suite.repo.On("GetUserByEmail", mock.Anything, email).
+		Return(nil, nil)
+
+	suite.repo.On("CreateUser", mock.Anything, mock.MatchedBy(func(u *model.User) bool {
+		return u.Name == name && u.Email == email && len(u.ID) > 0 && len(u.Password) > 0
+	})).Return(func(_ context.Context, u *model.User) *model.User {
+		return u
+	}, nil)
+
+	ctx := context.Background()
+
+	result, err := suite.service.RegisterUser(ctx, name, email, plainPassword)
+
+	suite.NoError(err)
+	suite.NotNil(result)
+	suite.Equal(name, result.Name)
+	suite.Equal(email, result.Email)
+	suite.NotEmpty(result.ID)
+	suite.NotEmpty(result.Password)
+	suite.NotEqual(plainPassword, result.Password)
+
+	suite.repo.AssertExpectations(suite.T())
+}
+
+func (suite *TestSuite) TestRegisterUser_Invalid_Input() {
+	name := ""
+	email := "noatsign"
+	plainPassword := "<8"
+
+	ctx := context.Background()
+
+	result, err := suite.service.RegisterUser(ctx, name, email, plainPassword)
+
+	suite.Nil(result)
+
+	svcError, ok := err.(*errs.ServiceError)
+
+	suite.True(ok)
+	suite.Equal(errs.ErrValidation, svcError.Code)
+
+	expectedMessage := "name is required; email is invalid; password is too short"
+	suite.Equal(expectedMessage, svcError.Message)
+
+	suite.Equal(map[string]any{"email": "noatsign"}, svcError.Context)
+
+	suite.repo.AssertExpectations(suite.T())
+}
+
+func (suite *TestSuite) TestRegisterUser_Fail_To_Check_Existing_User() {
+	name := "Valid Name"
+	email := "valid@gmail.com"
+	plainPassword := "password"
+
+	suite.repo.On("GetUserByEmail", mock.Anything, email).Return(nil, errors.New("any error"))
+
+	ctx := context.Background()
+
+	result, err := suite.service.RegisterUser(ctx, name, email, plainPassword)
+
+	suite.Nil(result)
+
+	svcError, ok := err.(*errs.ServiceError)
+
+	suite.True(ok)
+
+	suite.Equal(errs.ErrInternal, svcError.Code)
+	suite.Equal("failed to check existing user", svcError.Message)
+	suite.Equal(map[string]any{"email": "valid@gmail.com"}, svcError.Context)
+
+	suite.repo.AssertExpectations(suite.T())
+}
+
+func (suite *TestSuite) TestRegisterUser_Email_Exists() {
+	name := "Valid Name"
+	email := "valid@gmail.com"
+	plainPassword := "password"
+
+	existingUser := &model.User{
+		ID:   "Made up",
+		Name: "Vaild Name",
+	}
+
+	suite.repo.On("GetUserByEmail", mock.Anything, email).Return(existingUser, nil)
+
+	ctx := context.Background()
+	result, err := suite.service.RegisterUser(ctx, name, email, plainPassword)
+
+	suite.Nil(result)
+
+	svcError, ok := err.(*errs.ServiceError)
+
+	suite.True(ok)
+
+	suite.Equal(errs.ErrAlreadyExists, svcError.Code)
+	suite.Equal("email already exists", svcError.Message)
+	suite.Equal(map[string]any{"email": email, "user": existingUser}, svcError.Context)
+
+	suite.repo.AssertExpectations(suite.T())
+}
+
+func (suite *TestSuite) TestRegisterUser_CreateUser_Error_Propagated() {
+	email := "valid@gmail.com"
+	name := "Valid Name"
+	plainPassword := "8characters"
+
+	suite.repo.On("GetUserByEmail", mock.Anything, email).
+		Return(nil, nil)
+
+	propError := errs.NewInternalError("testing error propagation", errors.New("any error"), nil)
+
+	suite.repo.On("CreateUser", mock.Anything, mock.Anything).Return(nil, propError)
+
+	ctx := context.Background()
+
+	result, err := suite.service.RegisterUser(ctx, name, email, plainPassword)
+
+	suite.Nil(result)
+
+	svcError, ok := err.(*errs.ServiceError)
+
+	suite.True(ok)
+
+	suite.Equal(errs.ErrInternal, svcError.Code)
+	suite.Equal("testing error propagation", svcError.Message)
+	suite.Nil(svcError.Context)
+
+	suite.repo.AssertExpectations(suite.T())
+}
+
+func (suite *TestSuite) TestRegisterUser_Fail_To_Create_User() {
+	email := "valid@gmail.com"
+	name := "Valid Name"
+	plainPassword := "8characters"
+	anyError := errors.New("any error")
+
+	suite.repo.On("GetUserByEmail", mock.Anything, email).
+		Return(nil, nil)
+
+	suite.repo.On("CreateUser", mock.Anything, mock.Anything).Return(nil, anyError)
+
+	ctx := context.Background()
+
+	result, err := suite.service.RegisterUser(ctx, name, email, plainPassword)
+
+	suite.Nil(result)
+
+	svcError, ok := err.(*errs.ServiceError)
+
+	suite.True(ok)
+
+	suite.Equal(errs.ErrInternal, svcError.Code)
+	suite.Equal("failed to create user", svcError.Message)
+
+	expectedError := errs.NewInternalError("failed to create user", anyError, map[string]any{}).Error()
+
+	suite.Equal(expectedError, svcError.Error())
+	suite.Equal(map[string]any{}, svcError.Context)
+
+	suite.repo.AssertExpectations(suite.T())
 }
 
 func TestServiceRegisterUser(t *testing.T) {
