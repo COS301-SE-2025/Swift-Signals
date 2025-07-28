171a686ebe009444b89b2fc5824d58c5459da01f
Shravan Seebran <shravanseebran@gmail.com>
2025-07-28 16:35:35 +0200

    Fixed conversion errors

--- simulation-service/server/server.py
+++ simulation-service/server/server.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from concurrent import futures
 
 from google.protobuf.json_format import MessageToDict, ParseDict
@@ -6,7 +7,7 @@ from grpc_reflection.v1alpha import reflection
 import simulation_pb2 as pb
 import simulation_pb2_grpc as pb_grpc
 
-from simload import service
+import SimLoad
 
 
 class SimulationServicer(pb_grpc.SimulationServiceServicer):
@@ -14,14 +15,30 @@ class SimulationServicer(pb_grpc.SimulationServiceServicer):
         print("GetSimulationResults request received with id:",
               request.intersection_id)
 
-        req_dict = MessageToDict(request)
-        print(req_dict)
-        print("-------------------------------------------")
-        print(service.run_simulation(req_dict))
-        print("===========================================")
-        results = service.run_simulation(
-            req_dict)["summary"]["intersection"]["results"]
+        req_dict = {
+            "intersection": MessageToDict(request,
+                                          preserving_proto_field_name=True,
+                                          use_integers_for_enums=True)
+        }
+        results = SimLoad.main(
+            req_dict)[0]["intersection"]["results"]
         msg_results = pb.SimulationResultsResponse()
+        ParseDict(results, msg_results)
+
+        return msg_results
+
+    def GetSimulationOutput(self, request, context):
+        print("GetSimulationOutput request received with id:",
+              request.intersection_id)
+
+        req_dict = {
+            "intersection": MessageToDict(request,
+                                          preserving_proto_field_name=True,
+                                          use_integers_for_enums=True)
+        }
+        results = SimLoad.main(
+            req_dict)[1]
+        msg_results = pb.SimulationOutputResponse()
         ParseDict(results, msg_results)
 
         return msg_results
