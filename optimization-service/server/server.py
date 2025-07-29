da7fcc1a34cbac9360b8fe2b7d3a5ad9502490a6
Shravan Seebran <shravanseebran@gmail.com>
2025-07-29 16:34:15 +0200

    Fix bugs

--- optimization-service/server/server.py
+++ optimization-service/server/server.py
@@ -3,6 +3,7 @@ from concurrent import futures
 from google.protobuf.json_format import MessageToDict, ParseDict
 
 import grpc
+from grpc_reflection.v1alpha import reflection
 import optimisation_pb2 as pb
 import optimisation_pb2_grpc as pb_grpc
 
@@ -11,7 +12,8 @@ class OptimisationServicer(pb_grpc.OptimisationServiceServicer):
     def RunOptimisation(self, request, context):
         print("RunOptimisationResult request received with intersection_type:",
               request.parameters.intersection_type)
-        print(MessageToDict(request))
+        print(MessageToDict(request, preserving_proto_field_name=True,
+              use_integers_for_enums=True))
         return request
 
 
@@ -19,7 +21,14 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     pb_grpc.add_OptimisationServiceServicer_to_server(
         OptimisationServicer(), server)
-    server.add_insecure_port("localhost:50054")
+
+    SERVICE_NAMES = (
+        pb.DESCRIPTOR.services_by_name["OptimisationService"].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(SERVICE_NAMES, server)
+
+    server.add_insecure_port("[::]:50054")
     server.start()
     print("Optimisation Service listening on port :50054")
     server.wait_for_termination()
