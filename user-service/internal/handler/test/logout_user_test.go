33cc1097e381543b08582bbf6563a48ee1151858
Chris <christopherxides@gmail.com>
2025-08-10 12:23:13 +0200

    Implement unit tests for user-service Handler layer

---
+++ user-service/internal/handler/test/logout_user_test.go
@@ -0,0 +1,58 @@
+package test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	userpb "github.com/COS301-SE-2025/Swift-Signals/protos/gen/user"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (suite *TestSuite) TestLogoutUser_Success() {
+	req := &userpb.UserIDRequest{
+		UserId: "valid-user-id",
+	}
+
+	ctx := context.Background()
+
+	suite.service.On("LogoutUser", ctx, req.UserId).
+		Return(nil)
+
+	result, err := suite.handler.LogoutUser(ctx, req)
+
+	suite.Require().NoError(err)
+	suite.NotNil(result)
+
+	suite.service.AssertExpectations(suite.T())
+}
+
+func (suite *TestSuite) TestLogoutUser_Failure() {
+	req := &userpb.UserIDRequest{
+		UserId: "invalid-user-id",
+	}
+
+	suite.service.On("LogoutUser", mock.Anything, req.GetUserId()).
+		Return(errors.New("user not found"))
+
+	ctx := context.Background()
+
+	result, err := suite.handler.LogoutUser(ctx, req)
+
+	suite.Nil(result)
+	suite.Require().Error(err)
+
+	st, ok := status.FromError(err)
+	suite.True(ok)
+	suite.Equal(codes.Internal, st.Code())
+	suite.Equal("internal server error", st.Message())
+
+	suite.service.AssertExpectations(suite.T())
+}
+
+func TestHandlerLogoutUser(t *testing.T) {
+	suite.Run(t, new(TestSuite))
+}
