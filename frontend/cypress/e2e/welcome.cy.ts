14bd7c4323de94ea41c49746ed5a8d9a38487a0d
KgosiSegale <u22534718@tuks.co.za>
2025-08-12 18:18:09 +0200

    Update welcome.cy.ts

--- frontend/cypress/e2e/welcome.cy.ts
+++ frontend/cypress/e2e/welcome.cy.ts
@@ -1,1 +1,21 @@
+describe("Welcome Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/"); // Or use env baseUrl
+  });
+
+  it("should display the heading and logo", () => {
+    cy.contains("Welcome to Swift Signals!").should("exist");
+    cy.get('img[alt="Logo"]').should("be.visible");
+  });
+
+  it("should navigate to Login page on Login button click", () => {
+    cy.contains("Login").click();
+    cy.url().should("include", "/login");
+  });
+
+  it("should navigate to Register page on Register button click", () => {
+    cy.contains("Register").click();
+    cy.url().should("include", "/signup");
+  });
+});
 
