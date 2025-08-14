ebcf990af245c90a067c21c62f4cb2cde3fd99cc
KgosiSegale <u22534718@tuks.co.za>
2025-08-14 17:25:02 +0200

    Update login.cy.ts

--- frontend/cypress/e2e/login.cy.ts
+++ frontend/cypress/e2e/login.cy.ts
@@ -1,1 +1,17 @@
+describe("Login Page", () => {
+  beforeEach(() => {
+    cy.visit("/login"); // Assumes routing and dev server are ready
+  });
+
+  it("renders logo and form elements", () => {
+    cy.get('img[alt="Swift Signals Logo"]').should("be.visible");
+    cy.contains("Welcome to Swift Signals").should("exist");
+    cy.contains("Login").should("exist");
+    cy.get("input[name='username']").should("exist");
+    cy.get("input[name='password']").should("exist");
+    cy.contains("Log Me In").should("exist");
+  });
+
+  
+});
 
