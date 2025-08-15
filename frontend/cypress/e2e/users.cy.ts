59d7987ae76b0bb0094ab43e14bef94457554341
KgosiSegale <u22534718@tuks.co.za>
2025-08-15 12:18:45 +0200

    testing rendering

--- frontend/cypress/e2e/users.cy.ts
+++ frontend/cypress/e2e/users.cy.ts
@@ -1,1 +1,13 @@
+describe("Users Page", () => {
+  beforeEach(() => {
+    cy.visit("/users"); // Adjust path as necessary
+  });
+
+  it("renders a table with 9 user rows", () => {
+    cy.get("table.usersTable").should("exist");
+    cy.get("table.usersTable tbody tr").should("have.length", 9);
+  });
+
+  
+});
 
