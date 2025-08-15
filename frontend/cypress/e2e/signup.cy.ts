6ec2f3e23906c7b957e24e714f0574b205949cc9
KgosiSegale <u22534718@tuks.co.za>
2025-08-15 12:09:29 +0200

    rendering tests

--- frontend/cypress/e2e/signup.cy.ts
+++ frontend/cypress/e2e/signup.cy.ts
@@ -1,1 +1,17 @@
+describe("SignUp Page", () => {
+  beforeEach(() => {
+    cy.visit("/signup"); // Adjust if route is different
+  });
+
+  it("renders logo, title, and input fields", () => {
+    cy.get('img[alt="Swift Signals Logo"]').should("be.visible");
+    cy.contains("Welcome to Swift Signals").should("exist");
+    cy.contains("Sign Up").should("exist");
+    cy.get("input[name='username']").should("exist");
+    cy.get("input[name='email']").should("exist");
+    cy.get("input[name='password']").should("exist");
+    cy.contains("Register").should("exist");
+  });
+
+});
 
