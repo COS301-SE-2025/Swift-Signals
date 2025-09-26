e67d1d7ae94c68e4eb56766262f7c3b0c0642bb2
KgosiSegale <u22534718@tuks.co.za>
2025-09-26 21:18:24 +0200

    Update intersection.cy.ts

--- frontend/cypress/e2e/intersection.cy.ts
+++ frontend/cypress/e2e/intersection.cy.ts
@@ -103,7 +103,7 @@ describe("Intersections Page", () => {
           simulation_parameters: {
             intersection_type: "trafficlight",
             green: 20,
-            yellow: ,
+            yellow: 3,
             
           },
         },
