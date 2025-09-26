b9869e53f90f1f46497636485db9f9541ec354c5
KgosiSegale <u22534718@tuks.co.za>
2025-09-26 22:21:14 +0200

    Update users.cy.ts

--- frontend/cypress/e2e/users.cy.ts
+++ frontend/cypress/e2e/users.cy.ts
@@ -1,6 +1,6 @@
 describe("Users Page", () => {
-  beforeEach(() => {
     const API_BASE_URL = "http://localhost:9090";
+  beforeEach(() => {
     cy.visit("/users"); // Adjust path as necessary
   });
 
