48195a889a7f6110c0b675e46e93edb71919c52b
KgosiSegale <u22534718@tuks.co.za>
2025-09-26 22:19:50 +0200

    Update users.cy.ts

--- frontend/cypress/e2e/users.cy.ts
+++ frontend/cypress/e2e/users.cy.ts
@@ -1,7 +1,7 @@
 describe("Users Page", () => {
-  beforeEach(() => {
-    cy.visit("/users"); // Adjust path as necessary
-  });
+  // beforeEach(() => {
+  //   cy.visit("/users"); // Adjust path as necessary
+  // });
 
   // it("renders a table with 9 user rows", () => {
   //   cy.get("table.usersTable").should("exist");
