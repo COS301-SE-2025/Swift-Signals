91a9a46afa1270c4e1e07df9fe6275910559ff32
KgosiSegale <u22534718@tuks.co.za>
2025-09-26 22:18:43 +0200

    Update users.cy.ts

--- frontend/cypress/e2e/users.cy.ts
+++ frontend/cypress/e2e/users.cy.ts
@@ -17,14 +17,14 @@ describe("Users Page", () => {
       .should("exist");
   });
 
-  it("has working edit and delete buttons for each row", () => {
-    cy.get("table.usersTable tbody tr").each(($row) => {
-      cy.wrap($row).within(() => {
-        cy.get("button[aria-label='Edit user']").should("exist").click();
-        cy.get("button[aria-label='Delete user']").should("exist").click();
-      });
-    });
-  });
+  // it("has working edit and delete buttons for each row", () => {
+  //   cy.get("table.usersTable tbody tr").each(($row) => {
+  //     cy.wrap($row).within(() => {
+  //       cy.get("button[aria-label='Edit user']").should("exist").click();
+  //       cy.get("button[aria-label='Delete user']").should("exist").click();
+  //     });
+  //   });
+  // });
 
   // it("paginates to the next page", () => {
   //   cy.get("tbody tr").first().should("contain", "John Doe"); // Page 1
