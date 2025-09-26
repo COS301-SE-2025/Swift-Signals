5501e59b6a08ee3afc4f1612cbccdb955f1f23a2
KgosiSegale <u22534718@tuks.co.za>
2025-09-26 19:58:01 +0200

    Update intersection.cy.ts

--- frontend/cypress/e2e/intersection.cy.ts
+++ frontend/cypress/e2e/intersection.cy.ts
@@ -4,7 +4,8 @@ describe("Intersections Page", () => {
   const API_BASE_URL = "http://localhost:9090";
 
   beforeEach(() => {
-    
+    // Stub auth token in localStorage
+    window.localStorage.setItem("authToken", "fake-jwt-token");
   });
 });
 
