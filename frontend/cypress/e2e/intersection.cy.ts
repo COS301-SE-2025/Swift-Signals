ff54fa87900440e1c5ff47856f785ea69a709f82
KgosiSegale <u22534718@tuks.co.za>
2025-09-26 20:14:19 +0200

    Update intersection.cy.ts

--- frontend/cypress/e2e/intersection.cy.ts
+++ frontend/cypress/e2e/intersection.cy.ts
@@ -45,7 +45,7 @@ describe("Intersections Page", () => {
     cy.get("input[name='name']").type("New Intersection");
     cy.get("input[name='details.address']").type("Corner of Lynnwood & Atterbury");;
     cy.get("input[name='details.city']").clear().type("Pretoria");
-    cy.get("input[name='details.province']").clear().type("Pretoria");
+    cy.get("input[name='details.province']").clear().type("Gauteng");
   });
 });
 
